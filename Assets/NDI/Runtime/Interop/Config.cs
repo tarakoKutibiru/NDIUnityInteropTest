1a1bab56df848cc00a67ae5a73e76315694a9d82
sena_kuzuoka <senakuzuoka@gmail.com>
2020-07-05 03:51:05 +0900

    add android NDI SDk

--- Assets/NDI/Runtime/Interop/Config.cs
+++ Assets/NDI/Runtime/Interop/Config.cs
@@ -1,15 +1,17 @@
-namespace NDI.Interop
+﻿namespace NDI.Interop
 {
     static class Config
     {
-    #if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
         public const string DllName = "Processing.NDI.Lib.x64";
-    #elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
         public const string DllName = "libndi.4";
-    #elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+#elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
         public const string DllName = "ndi";
-    #else
+#elif UNITY_ANDROID
+        public const string DllName = "ndi";
+#else
         public const string DllName = "__Internal";
-    #endif
+#endif
     }
 }
